7d9ab893cd21ac24d336fd53ec123a858817a3cc
vikramkumarsj <newbilearning@gmail.com>
2020-12-09 20:14:44 +0530

    Update Streamlit_PowerGrid_Demo.py

--- Code/StreamLit/Streamlit_PowerGrid_Demo.py
+++ Code/StreamLit/Streamlit_PowerGrid_Demo.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 import pycaret
 from pycaret.regression import *
 
-Model = load_model("/app/Superconductors/Model/GBR_VK")
+Model = load_model("Superconductors//Model//GBR_VK")
 
 image = Image.open('/app/Superconductors/Data/Shell_Image.jpg')
 st.image(image)
